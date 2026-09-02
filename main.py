0861567fa512fc8e45b5b81b137785c2536ffc83
Davi Ladeira Balsamao <220748192+davi-balsamao@users.noreply.github.com>
2026-09-02 15:38:02 -0300

    2. Classe TestResult

---
+++ main.py
@@ -0,0 +1,34 @@
+from test_case import TestCase
+from test_result import TestResult
+ 
+ 
+class MyTest(TestCase):
+ 
+    def set_up(self):
+        print('set_up')
+ 
+    def tear_down(self):
+        print('tear_down')
+ 
+    def test_a(self):
+        print('test_a')
+ 
+    def test_b(self):
+        print('test_b')
+ 
+    def test_c(self):
+        print('test_c')
+ 
+ 
+result = TestResult()
+ 
+test = MyTest('test_a')
+test.run(result)
+ 
+test = MyTest('test_b')
+test.run(result)
+ 
+test = MyTest('test_c')
+test.run(result)
+ 
+print(result.summary())
